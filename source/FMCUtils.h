58846c0666610e1818e41872df6151f61b0c9e3d
Serge Lamikhov-Center <Serge.Lamikhov@freescale.com>
2012-11-19 14:18:12 +0200

    Change license terms. All files are under MIT license now

--- source/FMCUtils.h
+++ source/FMCUtils.h
@@ -1,14 +1,29 @@
 /* =====================================================================
  *
- *  Copyright 2009, 2010, Freescale Semiconductor, Inc., All Rights Reserved.
+ * The MIT License (MIT)
+ * Copyright 2009, 2010, Freescale Semiconductor, Inc.
  *
- *  This file contains copyrighted material. Use of this file is restricted
- *  by the provisions of a Freescale Software License Agreement, which has
- *  either accompanied the delivery of this software in shrink wrap
- *  form or been expressly executed between the parties.
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
- *  File Name : FMCUtils.h
- *  Author    : Serge Lamikhov-Center
+ * File Name : FMCUtils.h
+ * Author    : Serge Lamikhov-Center
  *
  * ===================================================================*/
 
