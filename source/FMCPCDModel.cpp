400d4833b72c986c016e13b588fae41bbaafe0f6
Florin Iordache <r70177@freescale.com>
2014-04-03 23:07:55 +0800

    Disabled ENGR00305912 until is available in FMD

--- source/FMCPCDModel.cpp
+++ source/FMCPCDModel.cpp
@@ -2782,9 +2782,15 @@ CFMCModel::getNetCommFieldType( std::string fieldname )
     fields["ipv4.hchecksum"]      = NET_HEADER_FIELD_IPv4_CKSUM;
     fields["ipv4.src"]            = NET_HEADER_FIELD_IPv4_SRC_IP;
     fields["ipv4.dst"]            = NET_HEADER_FIELD_IPv4_DST_IP;
-    fields["ipv6.ver"]            = NET_HEADER_FIELD_IPv6_VER | NET_HEADER_FIELD_IPv6_TC;
-    fields["ipv6.tos"]            = NET_HEADER_FIELD_IPv6_VER | NET_HEADER_FIELD_IPv6_TC;
-    fields["ipv6.flabel"]         = NET_HEADER_FIELD_IPv6_FL;
+
+    //ENGR00305912:
+    //fields["ipv6.ver"]            = NET_HEADER_FIELD_IPv6_VER | NET_HEADER_FIELD_IPv6_TC;
+    //fields["ipv6.tos"]            = NET_HEADER_FIELD_IPv6_VER | NET_HEADER_FIELD_IPv6_TC;
+    //fields["ipv6.flabel"]         = NET_HEADER_FIELD_IPv6_FL;
+    //ENGR00303764:
+    fields["ipv6.ver"]            = NET_HEADER_FIELD_IPv6_VER | NET_HEADER_FIELD_IPv6_TC | NET_HEADER_FIELD_IPv6_FL;
+    fields["ipv6.tos"]            = NET_HEADER_FIELD_IPv6_VER | NET_HEADER_FIELD_IPv6_TC | NET_HEADER_FIELD_IPv6_FL;
+
     fields["ipv6.nexthdr"]        = NET_HEADER_FIELD_IPv6_NEXT_HDR;
     fields["ipv6.src"]            = NET_HEADER_FIELD_IPv6_SRC_IP;
     fields["ipv6.dst"]            = NET_HEADER_FIELD_IPv6_DST_IP;
@@ -2863,9 +2869,15 @@ CFMCModel::getNetCommFieldTypeStr( std::string fieldname )
     fields["ipv4.hchecksum"]      = "NET_HEADER_FIELD_IPv4_CKSUM";
     fields["ipv4.src"]            = "NET_HEADER_FIELD_IPv4_SRC_IP";
     fields["ipv4.dst"]            = "NET_HEADER_FIELD_IPv4_DST_IP";
-    fields["ipv6.ver"]            = "NET_HEADER_FIELD_IPv6_VER | NET_HEADER_FIELD_IPv6_TC";
-    fields["ipv6.tos"]            = "NET_HEADER_FIELD_IPv6_VER | NET_HEADER_FIELD_IPv6_TC";
-    fields["ipv6.flabel"]         = "NET_HEADER_FIELD_IPv6_FL";
+    
+    //ENGR00305912:
+    //fields["ipv6.ver"]            = "NET_HEADER_FIELD_IPv6_VER | NET_HEADER_FIELD_IPv6_TC";
+    //fields["ipv6.tos"]            = "NET_HEADER_FIELD_IPv6_VER | NET_HEADER_FIELD_IPv6_TC";
+    //fields["ipv6.flabel"]         = "NET_HEADER_FIELD_IPv6_FL";
+    //ENGR00303764:
+    fields["ipv6.ver"]            = "NET_HEADER_FIELD_IPv6_VER | NET_HEADER_FIELD_IPv6_TC | NET_HEADER_FIELD_IPv6_FL";
+    fields["ipv6.tos"]            = "NET_HEADER_FIELD_IPv6_VER | NET_HEADER_FIELD_IPv6_TC | NET_HEADER_FIELD_IPv6_FL";
+
     fields["ipv6.nexthdr"]        = "NET_HEADER_FIELD_IPv6_NEXT_HDR";
     fields["ipv6.src"]            = "NET_HEADER_FIELD_IPv6_SRC_IP";
     fields["ipv6.dst"]            = "NET_HEADER_FIELD_IPv6_DST_IP";
@@ -2931,7 +2943,7 @@ CFMCModel::isFullFieldForCC( std::string fieldName )
         "ipv4.ttl",
         "ipv6.ver",
         "ipv6.tos",
-        "ipv6.flabel",
+        //"ipv6.flabel", //ENGR00305912: with "ipv6.flabel"  / ENGR00303764: without "ipv6.flabel"
         "ipv6.nexthdr",
         "ipv6.dst",
         "ipv6.src",
